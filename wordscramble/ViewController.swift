6408bc16bb739c087c8301694fd2b4f18444ad24
BJ <bj@zo0d-2.local>
2019-05-06 08:30:59 -0400

    Init

---
+++ wordscramble/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  wordscramble
+//
+//  Created by BJ on 2019-05-06.
+//  Copyright © 2019 BJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController{
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
