12de8db1727727768793d4d3ea85c7e1444ed020
BJ <bj@zo0d-2.local>
2019-05-07 09:08:14 -0400

    Complete w/ challenges & bonus

--- wordscramble/ViewController.swift
+++ wordscramble/ViewController.swift
@@ -28,6 +28,8 @@ class ViewController: UITableViewController{
         // Do any additional setup after loading the view.
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(promptForAnswer))
+        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Start", style: .plain, target: self, action: #selector(startGame))
+        
         
         if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
             if let startWords = try? String(contentsOf: startWordsURL) {
@@ -42,14 +44,11 @@ class ViewController: UITableViewController{
         startGame()
     }
     
-    func startGame() {
+    
+    @objc func startGame() {
         title = allWords.randomElement()
         usedWords.removeAll(keepingCapacity: true)
         tableView.reloadData()
-    }
-    
-    func submit(_ answer: String) {
-        
     }
     
     @objc func promptForAnswer() {
@@ -65,5 +64,63 @@ class ViewController: UITableViewController{
         present(ac, animated:true)
     }
     
-}
+    func showErrorMessage(title: String, message: String) {
+        let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Ok", style: .default))
+        present(ac, animated:true)
+    }
 
+    
+    func submit(_ answer: String) {
+        let lowerAnswer = answer.lowercased()
+        
+        if isPossible(word: lowerAnswer) {
+            if isOriginal(word: lowerAnswer) {
+                if isReal(word: lowerAnswer) {
+                    usedWords.insert(answer.lowercased(), at: 0)
+                    
+                    let indexPath = IndexPath(row: 0, section: 0)
+                    tableView.insertRows(at: [indexPath], with: .automatic)
+                    
+                    return
+                } else {
+                    
+                    showErrorMessage(title: "Invalid Word", message: "Miss me with that weak shit")
+                }
+            } else {
+                showErrorMessage(title: "Repeat Word", message: "Zero originality")
+            }
+        } else {
+            showErrorMessage(title: "Word Not Possible", message: "\(answer.capitalized)? Do you even English?")
+        }
+    }
+    
+    func isPossible(word: String) -> Bool {
+        
+        guard var tempWord = title?.lowercased() else { return false }
+        
+        for letter in word {
+            if let position = tempWord.firstIndex(of: letter) {
+                tempWord.remove(at: position)
+            } else {
+                return false
+            }
+        }
+        return true
+    }
+    
+    func isOriginal(word: String) -> Bool {
+        return !usedWords.contains(word)
+    }
+    
+    func isReal(word: String) -> Bool {
+        if word.count < 3 || word == title {
+            return false
+        } else {
+            let check = UITextChecker()
+            let range = NSRange(location:0, length: word.utf16.count)
+            let misspelledRange = check.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+            return misspelledRange.location == NSNotFound
+        }
+    }
+}
